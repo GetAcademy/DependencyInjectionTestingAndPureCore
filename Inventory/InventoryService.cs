a0afec695be79a4326e87a5f08adc810de73c45b
GET Academy <terje@kolderup.net>
2026-06-25 08:34:25 +0200

    Initial commit

---
+++ Inventory/InventoryService.cs
@@ -0,0 +1,21 @@
+﻿namespace Inventory
+{
+    public static class InventoryService
+    {
+        public static bool Buy(string itemId, int quantity)
+        {
+            var fileName = $@"inventory\{itemId}.txt";
+
+            var stock = 0;
+            if (File.Exists(fileName)) stock = Convert.ToInt32(File.ReadAllText(fileName));
+
+            if (quantity > stock) return false;
+
+            var newStock = stock - quantity;
+
+            File.WriteAllText(fileName, newStock.ToString());
+
+            return true;
+        }
+    }
+}
---
+++ Inventory/Program.cs
@@ -0,0 +1,17 @@
+﻿using Inventory;
+
+Console.WriteLine("Enkelt lagersystem");
+Console.WriteLine();
+
+Console.Write("Hvilken vare vil du kjøpe? ");
+var itemId = Console.ReadLine();
+
+Console.Write("Hvor mange vil du kjøpe? ");
+var quantity = Convert.ToInt32(Console.ReadLine());
+
+var success = InventoryService.Buy(itemId, quantity);
+
+Console.WriteLine();
+
+var message = success ? "Kjøpet ble gjennomført." : "Det er ikke nok på lager.";
+Console.WriteLine(message);
