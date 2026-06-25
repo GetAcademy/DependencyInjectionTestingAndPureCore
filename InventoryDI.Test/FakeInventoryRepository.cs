ae150f2cc07849ccc9a200b42461941f69218fae
GET Academy <terje@kolderup.net>
2026-06-25 10:42:53 +0200

    x

---
+++ InventoryDI.Test/FakeInventoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using InventoryWithDependencyInjection;
+
+namespace InventoryDI.Test
+{
+    internal class FakeInventoryRepository : IInventoryRepository
+    {
+        public int Stock { get; private set; }
+        public int StockUpdated { get; private set; }
+
+        public FakeInventoryRepository(int stock)
+        {
+            Stock = stock;
+        }
+
+        public int GetStock(string itemId)
+        {
+            return Stock;
+        }
+
+        public void UpdateStock(string itemId, int stock)
+        {
+            Stock = stock;
+            StockUpdated++;
+        }
+    }
+}
---
+++ InventoryDI.Test/InventoryServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using InventoryWithDependencyInjection;
+
+namespace InventoryDI.Test
+{
+    public class Tests
+    {
+        [Test]
+        public void TestBuyLessThanStock()
+        {
+            // arrange
+            var repo = new FakeInventoryRepository(9);
+            var inventoryService = new InventoryService(repo);
+
+            // act
+            var isSuccess = inventoryService.Buy("1", 3);
+
+            // assert
+            Assert.That(isSuccess, Is.True);
+            Assert.That(repo.Stock, Is.EqualTo(6));
+            Assert.That(repo.StockUpdated, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestBuyMoreThanStock()
+        {
+            // arrange
+            var repo = new FakeInventoryRepository(2);
+            var inventoryService = new InventoryService(repo);
+
+            // act
+            var isSuccess = inventoryService.Buy("1", 3);
+
+            // assert
+            Assert.That(isSuccess, Is.False);
+            Assert.That(repo.Stock, Is.EqualTo(2));
+            Assert.That(repo.StockUpdated, Is.EqualTo(0));
+        }
+    }
+}
